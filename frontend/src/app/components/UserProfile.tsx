7ee5a43c46ec56eb6e0b996cfcaa8dd027e2be10
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-10-16 09:59:38 +0300

    add

--- frontend/src/app/components/UserProfile.tsx
+++ frontend/src/app/components/UserProfile.tsx
@@ -4,26 +4,18 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuth } from "@/context/AuthContext";
-// import AuthForm from "./auth/AuthForm";
 import UserProducts from "./UserProducts";
 
 const UserProfile = () => {
     const { user, loading } = useAuth();
     const router = useRouter();
-    console.log("UserProfile👉user", user);
-    // useEffect(() => {
-    //     if (!loading && !user) {
-    //         router.push("/register");
-    //     }
-    // }, [user, loading, router]);
+    // console.log("UserProfile👉user", user);
 
     useEffect(() => {
-        if (loading) return; // ⛔️ Не перевіряємо поки триває запит
-        if (!user) router.push("/register");
+        if (loading) return;
+        if (!user) router.push("/login");
     }, [user, loading, router]);
 
-    // if (loading) return <p>Loading...</p>;
-    // if (!user) return <AuthForm type="register" />;
     if (loading) return <p className="text-center mt-10">Завантаження...</p>;
     if (!user) return null;
 
@@ -42,52 +34,3 @@ const UserProfile = () => {
 }
 
 export default UserProfile;
-
-
-// "use client";
-
-// import { useCallback } from "react";
-// import { getUserProfile } from "@/services/userService";
-// import { User } from "@/types/user";
-// import { getToken } from "@/utils/auth";
-// import AuthForm from "./auth/AuthForm";
-// import UserProducts from "./UserProducts";
-// import { useFetch } from "@/hooks/useFetch";
-// import AsyncState from "./common/AsyncState";
-
-// const UserProfile = () => {
-//     const token = getToken();
-
-//     const fetchUserProfile = useCallback(async () => {
-//         if (!token) throw new Error("No token");
-//         const response = await getUserProfile(token);
-//         return response.data;
-//     }, [token]);
-
-//     const { data: user, loading, error } = useFetch<User>(
-//         fetchUserProfile,
-//         { autoRun: Boolean(token), deps: [token] }
-//     );
-
-//     if (!token) return <AuthForm type="register" />;
-
-//     return (
-//         <>
-//             <AsyncState loading={loading} error={error} />
-
-//             {!loading && user && (
-//                 <div className="max-w-md mx-auto bg-white shadow-lg rounded-2xl p-6 mt-8">
-//                     <h1 className="text-2xl font-bold mb-4">👤 Profile</h1>
-//                     <p><strong>Name:</strong> {user.name}</p>
-//                     <p><strong>Email:</strong> {user.email}</p>
-//                     <p><strong>Role:</strong> {user.role}</p>
-//                 </div>
-//             )}
-
-//             {user && <UserProducts />}
-//         </>
-//     );
-// };
-
-// export default UserProfile;
-
